2ee808ede9a308755b52d7bcaf486244d72e7fef
Randy Taylor <tehgecKozzz@gmail.com>
2018-09-04 21:33:17 -0400

    [polybar] Automagically fetch monitors in launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch vegabar
-polybar vegabar &
+for m in $(polybar -m | cut -d ":" -f1); do
+    MONITOR=$m polybar --reload vegabar &
+done
