46565b4d8dbecfd40578533b285eedd7c363cd21
Randy Taylor <tehgecKozzz@gmail.com>
2019-06-21 20:49:15 -0400

    Update screenlayout

--- screenlayout/.screenlayout/default.sh
+++ screenlayout/.screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 1920x1080 --pos 3606x0 --rotate normal --output DP-3 --rate 144 --mode 1920x1080 --pos 1680x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DVI-D-1 --mode 1680x1050 --pos 0x0 --rotate normal
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --rate 144 --mode 1920x1080 --pos 1680x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 3600x0 --rotate normal --output DVI-D-0 --mode 1680x1050 --pos 0x0 --rotate normal
