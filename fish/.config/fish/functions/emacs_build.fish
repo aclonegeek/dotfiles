28003c31e50a82f6683f32e8f233b10c3999fdf1
Randy Taylor <dev@rjt.dev>
2022-10-23 21:03:19 -0400

    Fix emacs_build function

--- fish/.config/fish/functions/emacs_build.fish
+++ fish/.config/fish/functions/emacs_build.fish
@@ -1,10 +1,12 @@
 function emacs_build -a type clean
+    set -l nproc (math (nproc)/2)
+
     if test -n "$clean"
        git clean -xfd
        ./autogen.sh
     end
 
     emacs_configure $type
-    and make -j math (nproc)/2
+    make -j$nproc
     and sudo make install
 end
