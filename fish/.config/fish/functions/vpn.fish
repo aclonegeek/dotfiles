0b34439ccd9f432a66f2d33699b706a57adfa0b2
Randy Taylor <dev@rjt.dev>
2024-04-19 20:57:28 -0400

    Add vpn function to fish config

---
+++ fish/.config/fish/functions/vpn.fish
@@ -0,0 +1,31 @@
+function vpn -a state config
+    if begin; [ $state = "on" ]; and not test -n "$config"; end
+        echo "ERROR: Configuration unspecified"
+        return 1
+    end
+
+    # Killswitch.
+    if not grep -q "PostUp" $config
+        sed -i '/DNS/a PostUp = iptables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT' $config
+        sed -i '/PostUp/a PreDown = iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT' $config
+    end
+
+    set -l ipv6_state
+    set -l wg_state
+
+    switch $state
+        case "on"
+            set ipv6_state 0
+            set wg_state "up"
+        case "off"
+            set ipv6_state 1
+            set wg_state "down"
+        case '*'
+            echo "ERROR: Invalid state '$state'"
+            return 1
+    end
+
+    sudo sysctl -w net.ipv6.conf.all.disable_ipv6=$ipv6_state
+
+    sudo wg-quick $wg_state $config
+end
