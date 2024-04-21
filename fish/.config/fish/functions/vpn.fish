5982155e181847082392c94bb92b218e10b8110b
Randy Taylor <dev@rjt.dev>
2024-04-21 15:21:31 -0400

    Move state check to the top in vpn function

--- fish/.config/fish/functions/vpn.fish
+++ fish/.config/fish/functions/vpn.fish
@@ -1,4 +1,16 @@
 function vpn -a state config
+    switch $state
+        case "on"
+            set disable_ipv6 1
+            set wg_state "up"
+        case "off"
+            set disable_ipv6 0
+            set wg_state "down"
+        case '*'
+            echo "ERROR: Invalid state '$state'"
+            return 1
+    end
+
     if begin; [ $state = "on" ]; and not test -n "$config"; end
         echo "ERROR: Configuration unspecified"
         return 1
@@ -12,18 +24,6 @@ function vpn -a state config
 
     set -l disable_ipv6
     set -l wg_state
-
-    switch $state
-        case "on"
-            set disable_ipv6 1
-            set wg_state "up"
-        case "off"
-            set disable_ipv6 0
-            set wg_state "down"
-        case '*'
-            echo "ERROR: Invalid state '$state'"
-            return 1
-    end
 
     sysctl -w net.ipv6.conf.all.disable_ipv6=$disable_ipv6
 
