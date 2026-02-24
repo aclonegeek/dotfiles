22390f8db18be8ab8a393badc48dd0d924a1b15a
Randy Taylor <dev@rjt.dev>
2026-02-23 21:23:56 -0500

    Fix sway bar.sh issues with capslock/numlock
    These files now seem to have 2 numbers in them (one on each line),
    so we now account for that.

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -2,14 +2,14 @@
 
 LAPTOP=0
 
-caps_active=$(cat /sys/class/leds/*capslock/brightness)
-num_active=$(cat /sys/class/leds/*numlock/brightness)
+caps_active=0
+num_active=0
 
 trap "capslock" SIGUSR1
 trap "numlock" SIGUSR2
 
 capslock() {
-    if [[ $(cat /sys/class/leds/*capslock/brightness) -eq 0 ]]; then
+    if [[ $(cat /sys/class/leds/*capslock/brightness | head -n 1) -eq 0 ]]; then
         caps_active=0
     else
         caps_active=1
@@ -17,7 +17,7 @@ capslock() {
 }
 
 numlock() {
-    if [[ $(cat /sys/class/leds/*numlock/brightness) -eq 0 ]]; then
+    if [[ $(cat /sys/class/leds/*numlock/brightness | head -n 1) -eq 0 ]]; then
         num_active=0
     else
         num_active=1
@@ -53,6 +53,9 @@ battery() {
 wifi() {
     echo " $(grep wlp3s0 /proc/net/wireless | awk '{ print int($3 * 100 / 70) }')%"
 }
+
+capslock
+numlock
 
 while true
 do
